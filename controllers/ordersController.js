03a50791abc9e71a1491e202c15b2ac08b1db6cb
choongnyeong6215 <choong6215@gmail.com>
2024-04-19 17:40:46 +0900

    주문 API 비동기 처리 완료

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -2,9 +2,6 @@ const mysql = require("mysql2/promise");
 const { StatusCodes } = require("http-status-codes");
 
 const addOrder = async (req, res) => {
-  const { items, delivery, totalQuantity, totalPrice, userId, firstBookTitle } =
-    req.body;
-
   // db connection
   const conn = await mysql.createConnection({
     host: "localhost",
@@ -14,48 +11,119 @@ const addOrder = async (req, res) => {
     dateStrings: true,
   });
 
-  // deliveries
-  const sql = `INSERT INTO deliveries (address, receiver, contact) VALUES (?, ?, ?)`;
-  const values = [delivery.address, delivery.receiver, delivery.contact];
+  const { items, delivery, totalQuantity, totalPrice, userId, firstBookTitle } =
+    req.body;
 
-  const [results] = await conn.query(sql, values);
+  try {
+    // deliveries
+    const newDeliveryQuery = `INSERT INTO deliveries (address, receiver, contact) VALUES (?, ?, ?)`;
 
-  const deliveryId = results.insertId;
+    const [deliveriseRes] = await conn.execute(newDeliveryQuery, [
+      delivery.address,
+      delivery.receiver,
+      delivery.contact,
+    ]);
 
-  // orders
-  // newDeliveryQuery = `INSERT INTO orders (book_title, total_price, total_quantity, user_id, delivery_id) VALUES (?, ?, ?, ?, ?)`;
-  // values = [firstBookTitle, totalQuantity, totalPrice, userId, deliveryId];
+    const deliveryId = deliveriseRes.insertId;
 
-  // conn.query(newDeliveryQuery, values, (err, results) => {
-  //   if (err) {
-  //     console.log(err);
-  //     return res.status(StatusCodes.BAD_REQUEST).end();
-  //   }
+    // orders
+    const newOrdersquery = `INSERT INTO orders (book_title, total_price, total_quantity, user_id, delivery_id) VALUES (?, ?, ?, ?, ?)`;
 
-  //   orderId = results.insertId;
-  // });
+    const [ordersRes] = await conn.execute(newOrdersquery, [
+      firstBookTitle,
+      totalQuantity,
+      totalPrice,
+      userId,
+      deliveryId,
+    ]);
 
-  // // orderedBook
-  // newDeliveryQuery = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`;
-  // values = [];
+    const orderId = ordersRes.insertId;
 
-  // items.forEach((item) => {
-  //   values.push([orderId, item.bookId, item.quantity]);
-  // });
+    // 주문할 상품 데이터 추출
+    const toOrderQuery = `SELECT book_id, quantity FROM cartItems WHERE id IN (?)`;
+    const [orderItems, fields] = await conn.query(toOrderQuery, [items]);
 
-  // conn.query(newDeliveryQuery, [values], (err, results) => {
-  //   if (err) {
-  //     console.log(err);
-  //     return res.status(StatusCodes.BAD_REQUEST).end();
-  //   }
+    // orderedBook
+    const newOrderedBookQuery = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`;
+    const values = [];
 
-  //   return res.status(StatusCodes.OK).json(results);
-  // });
+    orderItems.forEach((orderItem) => {
+      values.push([orderId, orderItem.book_id, orderItem.quantity]);
+    });
+
+    const orderedBookRes = await conn.query(newOrderedBookQuery, [values]);
+
+    // 주문 상품 장바구니에서 삭제
+    const deleteCartItemsRes = await deleteCartItems(conn, items);
+
+    return res.status(StatusCodes.OK).json(deleteCartItemsRes);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-const getOrders = (req, res) => {};
+// 주문 상품 장바구니에서 삭제
+const deleteCartItems = async (conn, items) => {
+  try {
+    const deleteCartItemsQuery = `DELETE FROM cartItems WHERE id IN (?)`;
+    return await conn.query(deleteCartItemsQuery, [items]);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const getOrderDetail = (req, res) => {};
+const getOrders = async (req, res) => {
+  // db connection
+  const conn = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "Bookstore",
+    dateStrings: true,
+  });
+
+  try {
+    const orderByUsers = `
+      select o.id, o.created_at, d.address, d.receiver, d.contact, o.book_title, o.total_price, o.total_quantity
+      from orders o
+      left join deliveries d
+      on o.delivery_id = d.id;`;
+
+    const [rows, fields] = await conn.query(orderByUsers);
+
+    return res.status(StatusCodes.OK).json(rows);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getOrderDetail = async (req, res) => {
+  // db connection
+  const conn = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "Bookstore",
+    dateStrings: true,
+  });
+
+  try {
+    const { id } = req.params;
+
+    const getOrderDetailQuery = `
+      SELECT b.id, b.title, b.author, b.price, o.quantity
+      FROM orderedBook o
+      LEFT JOIN books b
+      ON o.book_id = b.id
+      WHERE o.id = ?`;
+
+    const [rows, fields] = await conn.query(getOrderDetailQuery, id);
+
+    return res.status(StatusCodes.OK).json(rows);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   addOrder,
