b2ecc876cd85c64caaa4f355776c38dae73a6ef3
choongnyeong6215 <choong6215@gmail.com>
2024-04-23 16:33:34 +0900

    토큰 없는 경우 예외처리

--- middlewares/ensureAuthorization.js
+++ middlewares/ensureAuthorization.js
@@ -6,11 +6,20 @@ const ensureAuthorization = (req) => {
   try {
     const receivedJWT = req.headers["authorization"];
 
-    const decodedJWT = jwt.verify(receivedJWT, process.env.PRIVATE_KEY);
+    console.log(`receivedJWT : ${receivedJWT}`);
 
-    return decodedJWT;
+    if (receivedJWT) {
+      const decodedJWT = jwt.verify(receivedJWT, process.env.PRIVATE_KEY);
+
+      return decodedJWT;
+    }
+    // 토큰 없는 경우 고려
+    else {
+      throw new ReferenceError("jwt must be provided");
+    }
   } catch (err) {
-    console.log(err);
+    console.log(err.name);
+    console.log(err.message);
 
     return err;
   }
