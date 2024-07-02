e1066d0fd1c600d3f3854da49c8ba1f714f7a2b0
choongnyeong6215 <choong6215@gmail.com>
2024-07-02 21:44:48 +0900

    cors 설정

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,26 +1,38 @@
-import express, { Application } from 'express';
-import dotenv from 'dotenv';
-import usersRouter from './routes/users.router';
-import categoriesRouter from './routes/categories.router';
-import booksRouter from './routes/books.router';
-import likesRouter from './routes/likes.router';
-import cartItemsRouter from './routes/cartItems.router';
-import orderRouter from './routes/orders.router';
+import express, { Application } from "express";
+import dotenv from "dotenv";
+import usersRouter from "./routes/users.router";
+import categoriesRouter from "./routes/categories.router";
+import booksRouter from "./routes/books.router";
+import likesRouter from "./routes/likes.router";
+import cartItemsRouter from "./routes/cartItems.router";
+import orderRouter from "./routes/orders.router";
+import { CorsOptions } from "cors";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Application = express();
 
+const corsOptions: CorsOptions = {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
 // body-parsing
 app.use(express.json());
 
+// cors
+app.use(cors(corsOptions));
+
 // route handler
-app.use('/users', usersRouter);
-app.use('/categories', categoriesRouter);
-app.use('/books', booksRouter);
-app.use('/likes', likesRouter);
-app.use('/carts', cartItemsRouter);
-app.use('/orders', orderRouter);
+app.use("/users", usersRouter);
+app.use("/categories", categoriesRouter);
+app.use("/books", booksRouter);
+app.use("/likes", likesRouter);
+app.use("/carts", cartItemsRouter);
+app.use("/orders", orderRouter);
 
 app.listen(process.env.BASE_PORT || 8080, () =>
   console.log(`book-store is running on ${process.env.BASE_PORT} port`)
