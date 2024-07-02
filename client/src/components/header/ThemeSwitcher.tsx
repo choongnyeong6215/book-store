16c322406a38a82d85bbb270b3bd992bd4192e62
choongnyeong6215 <choong6215@gmail.com>
2024-07-02 19:27:23 +0900

    테마 변경 컴포넌트 적용

---
+++ client/src/components/header/ThemeSwitcher.tsx
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import { IThemeContext, themeContext } from "../../context/ThemeContext";
+
+const ThemeSwitcher = () => {
+  const { themeName, toggleTheme } = useContext <IThemeContext>(themeContext);
+
+  return <button onClick={toggleTheme}>{themeName}</button>;
+};
+
+export default ThemeSwitcher;
