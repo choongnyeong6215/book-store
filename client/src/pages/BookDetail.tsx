f2e956187feb6c9a542ebd6f678b8733cb17263f
choongnyeong6215 <choong6215@gmail.com>
2024-07-04 22:03:58 +0900

    도서 페이지 구현

---
+++ client/src/pages/BookDetail.tsx
@@ -0,0 +1,138 @@
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { useBook } from "../hooks/useBook";
+import { generateImg } from "../utils/image";
+import Title from "../components/common/Title";
+import { IBookDetail } from "../models/books.model";
+import { formatDate, formatNumber } from "../utils/format";
+import { Link } from "react-router-dom";
+import ElipsisBox from "../components/common/ElipsisBox";
+import LikeButton from "../components/book/LikeButton";
+import AddToCart from "../components/book/AddToCart";
+
+const bookInfoList = [
+  {
+    label: "카테고리",
+    key: "categoryName",
+    filter: (book: IBookDetail) => (
+      <Link to={`/books?categoryId=${book.categoryId}`}>
+        {book.categoryName}
+      </Link>
+    ),
+  },
+  {
+    label: "포맷",
+    key: "form",
+  },
+  {
+    label: "페이지",
+    key: "pages",
+  },
+  {
+    label: "ISBN",
+    key: "isbn",
+  },
+  {
+    label: "출간일",
+    key: "pubDate",
+    filter: (book: IBookDetail) => {
+      return formatDate(book.pubDate, "YYYY년 MM월 DD일");
+    },
+  },
+  {
+    label: "가격",
+    key: "price",
+    filter: (book: IBookDetail) => {
+      return `${formatNumber(book.price)}원`;
+    },
+  },
+];
+
+const BookDetail = () => {
+  const { bookId } = useParams();
+  const { book, likeToggle } = useBook(bookId);
+
+  if (!book) return null;
+
+  return (
+    <BookDetailStyle>
+      <header className="header">
+        <div className="img">
+          <img src={generateImg(book.img)} alt={book.title} />
+        </div>
+
+        <div className="info">
+          <Title size="large" color="text">
+            {book.title}
+          </Title>
+          {bookInfoList.map((info) => (
+            <dl key={info.key}>
+              <dt>{info.label}</dt>
+              <dd>
+                {info.filter
+                  ? info.filter(book)
+                  : book[info.key as keyof IBookDetail]}
+              </dd>
+            </dl>
+          ))}
+          <p className="summary">{book.summary}</p>
+
+          <div className="like">
+            <LikeButton book={book} onClick={likeToggle} />
+          </div>
+
+          <div className="add-cart">
+            <AddToCart book={book} />
+          </div>
+        </div>
+      </header>
+      <div className="content">
+        <Title size="medium">상세 설명</Title>
+        <ElipsisBox limitLine={4}>{book.detail}</ElipsisBox>
+
+        <Title size="medium">목차</Title>
+        <p className="index">{book.contents}</p>
+      </div>
+    </BookDetailStyle>
+  );
+};
+
+const BookDetailStyle = styled.div`
+  header {
+    display: flex;
+    align-items: start;
+    gap: 24px;
+    padding: 0 0 24px 0;
+
+    .img {
+      flex: 1;
+      img {
+        width: 100%;
+        height: auto;
+      }
+    }
+    .info {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      gap: 12px;
+    }
+
+    dl {
+      display: flex;
+      margin: 0;
+      dt {
+        width: 80px;
+        color: ${({ theme }) => theme.color.secondary};
+      }
+      a {
+        color: ${({ theme }) => theme.color.primary};
+      }
+    }
+  }
+
+  .content {
+  }
+`;
+
+export default BookDetail;
